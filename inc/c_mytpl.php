37b5b5277548d543a922434e60a950661aa6dbfd
JWhy <jwhy@jwhy.de>
2013-10-31 17:49:04 +0100

    Added page title fall back in MyTPL class
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- inc/c_mytpl.php
+++ inc/c_mytpl.php
@@ -6,6 +6,7 @@ class MyTPL extends RainTPL{
     $this->assign('username', (
         (isset($_SESSION['mcname'])? $_SESSION['mcname'] : false)
     ));
+    if(!isset($this->var['title'])) $this->assign('title', 'Page');
     parent::draw('header');
     parent::draw($tpl_name, $return_string);
     parent::draw('footer');
