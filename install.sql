cae5474d44c6bf81f94d061200d405c1c8ac75c1
JWhy <jwhy@jwhy.de>
2013-10-31 16:07:38 +0100

    Add install.sql
    Signed-off-by: JWhy <jwhy@jwhy.de>

---
+++ install.sql
@@ -0,0 +1,40 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `items`
+-- ----------------------------
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE `items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL DEFAULT 'New Product',
+  `description` text,
+  `command` varchar(75) DEFAULT NULL,
+  `color` varchar(6) DEFAULT '2C4985',
+  `price` float NOT NULL,
+  `active` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='item data';
+
+-- ----------------------------
+-- Records of items
+-- ----------------------------
+INSERT INTO `items` VALUES ('1', 'VIP', 'Very Important Player whatsoever', 'pex user {u} group set {g}', 'CC0066', '10', '1');
+INSERT INTO `items` VALUES ('2', 'VIP+', 'Very Very Important Player', 'pex user {u} group set vipplus', '660033', '15', '1');
+INSERT INTO `items` VALUES ('3', 'VIP++', 'Incredibly Important', 'pex user {u} group set vipplusplus', '660066', '25', '1');
+
+-- ----------------------------
+-- Table structure for `users`
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
+  `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name',
+  `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
+  `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
