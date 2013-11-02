81daa949eb837312702f86a5b22f30c76307f417
JWhy <jwhy@jwhy.de>
2013-11-02 01:47:12 +0100

    Catch errors on RainTPL rendering (e.g. missing write perms)
    Signed-off-by: JWhy <jwhy@jwhy.de>

--- inc/c_mytpl.php
+++ inc/c_mytpl.php
@@ -3,13 +3,17 @@
 class MyTPL extends RainTPL{
 
   function draw($tpl_name, $return_string = false){
-    $this->assign('username', (
-        (isset($_SESSION['mcname'])? $_SESSION['mcname'] : false)
-    ));
-    if(!isset($this->var['title'])) $this->assign('title', 'Page');
-    parent::draw('header');
-    parent::draw($tpl_name, $return_string);
-    parent::draw('footer');
+    try{
+      $this->assign('username', (
+          (isset($_SESSION['mcname'])? $_SESSION['mcname'] : false)
+      ));
+      if(!isset($this->var['title'])) $this->assign('title', 'Page');
+      parent::draw('header');
+      parent::draw($tpl_name, $return_string);
+      parent::draw('footer');
+    }catch(Exception $e){
+      die('Could not render page. Error: ' . $e->getMessage());
+    }
   }
   
 }
